0a976a088b70e1c1fee97e357b89ed1f1a6a3a40
gonzalofp340 <gonzalo.fp340@gmail.com>
2013-07-19 00:15:27 -0300

    first commit

---
+++ proxy_config.py
@@ -0,0 +1,11 @@
+# Web con lista de proxys para usar
+# http://hidemyass.com/proxy-list/
+
+#http_proxy = "http://189.126.198.3:8080"
+#ftp_proxy  = "http://189.126.198.3:8080"
+
+#http_proxy = "http://119.92.232.130:3128"
+#ftp_proxy  = "http://119.92.232.130:3128"
+
+http_proxy = ""
+ftp_proxy  = ""
